43f08d1149e387519bd9e1c134abb79f873abcf5
Jennifer Han <araam129@gmail.com>
2020-04-29 22:09:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
 function logWhisper(string) {
-  console.log(string.toLowerCase);
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  if(string === string.toLowerCase) {
+  if(string === string.toLowerCase()) {
     return "I can\'t hear you!";
-  } else if (string === string.toUpperCase) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma") {
     return "I love you, too."
